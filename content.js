1359f7dd864ba9900c4e36ecaa24c7e98c1b2517
Conor Murphy <cmurphy580@gmail.com>
2021-07-26 14:39:03 -0700

    Update to content.js

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 // KEYS
-const NETFLIX_DATA_API_KEY = "";
+const NETFLIX_DATA_API_KEY = "c60e12e22fmsh35462b12fa5c85cp19996ajsn96e349f583a0";
 
 const getNetflixData = async(id) => {
   // limited to 100 calls per day
@@ -27,6 +27,7 @@ const getNetflixData = async(id) => {
 
 const getRTResponse = async(url) => {
   // https://allorigins.win/ <-- proxy server api
+  console.log(url);
   const response = await fetch(`https://api.allorigins.win/get?url=${url}`).then(response => response.json()).then(data => data.contents).catch(err => false);
   if (response.includes("404 - Not Found") || response === false) {
     return false;
@@ -81,13 +82,13 @@ const getYearReleased = (response, data) => {
   if (data.type === "series") {
     console.log("series year: ");
     var container = response.getElementsByClassName("fgm")[1].nextElementSibling !== undefined ? response.getElementsByClassName("fgm")[1].nextElementSibling : 0;
-    year = container ? +container.innerText.split(" ")[2]  : 0;
+    year = container ? +container.innerText.split(" ")[2] : 0;
     console.log(year);
   } else {
     console.log("film year: ");
     var year1, year2;
-    year1 = response.querySelectorAll("time")[0] ? +response.querySelectorAll("time")[0].getAttribute("datetime").split("-")[0] : 3300;
-    year2 = response.querySelectorAll("time")[1] ? +response.querySelectorAll("time")[1].getAttribute("datetime").split("-")[0] : 3300;
+    year1 = response.querySelectorAll("time")[0] ? +response.querySelectorAll("time")[0].getAttribute("datetime").split(" ")[2] : 3300;
+    year2 = response.querySelectorAll("time")[1] ? +response.querySelectorAll("time")[1].getAttribute("datetime").split(" ")[2] : 3300;
     year = year1 <= year2 ? year1 : year2;
     console.log(year);
   }
@@ -102,18 +103,29 @@ const getRTScores = async(url, data, year = null) => {
   if (response !== false) {
     if (year === null) year = getYearReleased(response, data);
     if (Math.abs(year - data.year) > 1) return false;
-    var ratingsElement = response.getElementsByClassName("mop-ratings-wrap__percentage");
-    var critic = await ratingsElement[0] !== undefined ? ratingsElement[0].innerText.trim() : "N/A";
-    var audience = await ratingsElement[1] !== undefined ? ratingsElement[1].innerText.trim() : "N/A";
-    if (critic === "N/A" && audience === "N/A") return false;
-    ratings =  {
-      "title" : data.title,
-      "year" : data.year,
-      critic,
-      audience,
-      "imbd": data.imbd
+    console.log("response");
+    // console.log(response);
+    // console.log(+response.getElementsByClassName("mop-ratings-wrap__percentage")[0].innerText.split("%")[0]);
+    var ratingsElement = response.getElementsByClassName("scoreboard")[0];
+    var critic = await ratingsElement ? +ratingsElement.attributes.tomatometerscore.value : "N/A";
+    var audience = await ratingsElement? +ratingsElement.attributes.audiencescore.value : "N/A";
+    if (ratingsElement === undefined) {
+      ratingsElement = response.getElementsByClassName("mop-ratings-wrap__percentage");
+      critic = ratingsElement[0] ? +ratingsElement[0].innerText.split("%")[0] : "N/A";
+      audience = ratingsElement[1] ? +ratingsElement[1].innerText.split("%")[0] : "N/A";
+      console.log(`critic: ${critic}, audience: ${audience}`);
     }
-    return ratings === undefined ? false : ratings;
+    if (critic === "N/A" && audience === "N/A") return false;
+      ratings =  {
+        "title" : data.title,
+        "year" : data.year,
+        "critic" : `${critic}%`,
+        "audience" : `${audience}%`,
+        "imbd": data.imbd
+      }
+      console.log("ratings: ");
+      console.log(ratings);
+      return ratings === undefined ? false : ratings;
   }
   return false;
 };
@@ -190,96 +202,79 @@ const getRTData = async(data) => {
 
 const displayRTData = async(anchorTag, rTData, buttonPosition) => {
   anchorTag.innerHTML = "";
+  // anchorTag.classList.remove("ltr-pjs1vp");
+  anchorTag.style.cssText = "border-radius: 2px !important; padding: 0 0.8rem 0 0.8rem !important;"; // "display: flex; width: 100% !important; flex-wrap: wrap !important; border: 1px solid rgba(255, 255, 255, 0.7); color: white; border-radius: 2px; padding: 0 0.8rem 0 0.8rem;"
   var div = document.createElement("div");
-  div.style.cssText = "display: inline-flex; flex-direction: row; justifyContent: flex-end; align-items: center; height: inherit; width: auto;";
+  div.style.cssText = "display: flex; flex-direction: row; justifyContent: flex-end; align-items: center; height: inherit;";
   var criticImg = document.createElement("img"), audienceImg = document.createElement("img");
   criticImg.src = +rTData.critic.substring(0, rTData.critic.length - 1) >= 60 ? "https://vignette.wikia.nocookie.net/logopedia/images/2/20/New_Fresh.svg/revision/latest/scale-to-width-down/139?cb=20190317211435" : "https://vignette.wikia.nocookie.net/logopedia/images/5/52/Rotten_Tomatoes_rotten.svg/revision/latest/scale-to-width-down/145?cb=20190317223047";
-  criticImg.className = "svg-icon svg-icon-audio-off";
-  criticImg.style.cssText = "max-width: 14px; max-height: 14px; min-width: 14px; min-height: 14px; padding: 2px;";
+  // criticImg.className = "small ltr-dguo2f";
+  criticImg.style.cssText = "margin: 0 0.5rem 0 0.25rem; max-width: 15px; max-height: 15px; min-width: 15px; min-height: 15px;"; // "padding: 2px;
   audienceImg.src = +rTData.audience.substring(0, rTData.audience.length - 1) >= 60 ? "https://vignette.wikia.nocookie.net/logopedia/images/d/da/Rotten_Tomatoes_positive_audience.svg/revision/latest/scale-to-width-down/106?cb=20190317223058" : "https://vignette.wikia.nocookie.net/logopedia/images/6/63/Rotten_Tomatoes_negative_audience.svg/revision/latest/scale-to-width-down/144?cb=20190317223112";
-  audienceImg.className = "svg-icon svg-icon-audio-off";
-  audienceImg.style.cssText = "max-width: 14px; max-height: 14px; min-width: 14px; min-height: 14px; padding: 2px;"; // 1 rem/em === 16px, 0.15 rem/em === 2px
+  // audienceImg.className = "small ltr-dguo2f";
+  audienceImg.style.cssText = "margin: 0 0.5rem 0 0.25rem; max-width: 15px; max-height: 15px; min-width: 15px; min-height: 15px;"; // "padding: 2px; // 1 rem/em === 16px, 0.15 rem/em === 2px
   var criticRating = document.createElement("p"), audienceRating = document.createElement("p");
   criticRating.innerHTML = rTData.critic;
-  criticRating.style.cssText = "padding: 2px; fontSize: 12px";
+  criticRating.style.cssText = "fontSize: 0.6rem; margin: 0 1rem 0 0;" // "padding: 2px; fontSize: 10px";
   audienceRating.innerHTML = rTData.audience;
-  audienceRating.style.cssText = "padding: 2px; fontSize: 12px";
+  audienceRating.style.cssText = "fontSize: 0.6rem"; // "padding: 2px; fontSize: 10px";
   div.append(criticImg, criticRating, audienceImg, audienceRating);
   anchorTag.append(div);
-  anchorTag.style.cssText = `border-radius: 15px; width: fit-content; position: absolute; top: -${buttonPosition}px; right: 0;`; // -4.95em
+  // anchorTag.style.cssText = `border-radius: 15px; width: fit-content; position: absolute; top: -${buttonPositionY}px; right: ${buttonPositionX};`; // 0 , -4.95em
 };
 
 const appendRTButton = async(id, buttons) => {
   var outerDiv = document.createElement("div");
-  outerDiv.className = "global-supplemental-audio-toggle ButtonTransition-fade-enter ButtonTransition-fade-enter-active";// "global-supplemental-audio-toggle";
-  if (buttons !== null) {
-    buttons.prepend(outerDiv);
-    var innerDiv = document.createElement("div");
-    innerDiv.className = "nf-svg-button-wrapper";
-    outerDiv.append(innerDiv);
+  outerDiv.className = "ltr-79elbk"; // "color-supplementary hasIcon ltr-pjs1vp";
+  // if (buttons !== null) {
+    buttons.append(outerDiv);
+    // var innerDiv = document.createElement("div");
+    // outerDiv.append(innerDiv);
     var anchorTag = document.createElement("a");
-    anchorTag.className = "nf-svg-button simpleround";
-    anchorTag.style.cssText = "display: flex; flex-direction: row; justify-content: center; align-items: center;"
-    innerDiv.append(anchorTag);
+    anchorTag.className = "color-supplementary hasIcon ltr-pjs1vp";
+    anchorTag.style.cssText = "display: flex; flex-direction: row; justify-content: center; align-items: center;"// order: 5 !important; flex-wrap: wrap !important;"// width: 100 !important; flex-basis: 100% !important; flex-wrap: wrap !important;"
+    outerDiv.append(anchorTag);
     var imgTag = document.createElement("img");
-    imgTag.className = "svg-icon svg-icon-audio-off"; // "svg-icon";
     imgTag.src = "https://vignette.wikia.nocookie.net/logopedia/images/2/20/New_Fresh.svg/revision/latest/scale-to-width-down/139?cb=20190317211435";
     imgTag.style.cssText = "max-width: 18px; max-height: 18px; min-width: 14px; min-height: 14px;"
     anchorTag.append(imgTag);
-    var buttonPosition = outerDiv.getBoundingClientRect().height;
-    anchorTag.addEventListener("click", async function() {
+    // var buttonPositionY = outerDiv.getBoundingClientRect().height;
+    // var buttonPositionX = outerDiv.getBoundingClientRect().width;
+    anchorTag.addEventListener("click", async function(event) {
+      event.stopPropagation();
       // loading...
-      this.innerHTML = "...";
+      this.innerHTML = ". . .";
+      // console.log(id);
       // get Netflix Data
       const netflixData = await getNetflixData(id);
+      console.log(netflixData);
       // get Rotten Tomatoes Data
-      var rTData;
+      // var rTData;
       if (netflixData !== undefined) rTData = await getRTData(netflixData);
+      // var rTData = { "critic": "97%", "audience": "45%" };
       if (rTData !== undefined && rTData.hasOwnProperty("critic")) {
         console.log(rTData);
-        displayRTData(this, rTData, buttonPosition);
+        displayRTData(this, rTData); // , buttonPositionY, buttonPositionX);
       } else {
-        this.innerHTML = "?";
+        // ? == can't find data
+        this.innerHTML = "-?-";
       }
-      // displayRTData(this, rTData);
       console.log("finished loading");
     });
+  // }
+};
+
+// addEventListener
+window.addEventListener("click", function(event) {
+  var arr = window.location.href.split("=")
+  var id, buttons;
+  try {
+    console.log("...");
+    var id = arr[arr.length - 1];
+    var buttons = document.querySelector(".buttonControls--container");  // || document.querySelector(".bob-buttons-wrapper");
+    // console.log(id, buttons);
+    if (id && buttons) appendRTButton(id, buttons);
+  } catch(err) {
+    console.log(err);
   }
-}
-
-// Allows extension to observe changes to the dom.
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-// The change we wish to observe.
-var target = document.querySelector(".mainView");
-// Define an observer that looks for a specific change.
-var observer = new MutationObserver(async function(mutations, observer) {
-    // Fired when a mutation occurs.
-    var href, id, buttons;
-    try {
-      console.log("...");
-      var container = mutations.find(mutation => mutation.target.className.includes("is-bob-open")).target.children[1].children[0].children[0].children;
-      if (container.length === 3) {
-        href = container[1].children[1].href;
-        id = +href.split(".com")[1].split("/")[2];
-        buttons = container[1].children[3].firstChild;
-        setTimeout(appendRTButton, 1200, id, buttons);
-      } else if (container.length === 2) {
-        href = container[0].children[2].children[1].href;
-        id = +href.split(".com")[1].split("/")[2];
-        buttons = container[0].children[2].children[3].firstChild;
-        setTimeout(appendRTButton, 1200, id, buttons);
-      }
-    } catch(err) {
-      href = undefined;
-    }
-});
-
-// Define what element should be observed by the observer
-// and what types of mutations trigger the callback.
-// Main code that runs the observer.
-observer.observe(target, {
-  subtree: true,
-  attributes: true,
-  characterData: true,
-  characterDataOldValue: true
 });
